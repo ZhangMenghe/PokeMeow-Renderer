76f90edd36fd69d6030a578b743313b03eca3201
ZhangMenghe <zmh.leaves@gmail.com>
2017-06-18 23:59:03 -0700

    separate common parameter value

---
+++ src/main/java/org/cytoscape/pokemeow/internal/commonUtil.java
@@ -0,0 +1,13 @@
+package main.java.org.cytoscape.pokemeow.internal;
+
+/**
+ * Created by ZhangMenghe on 2017/6/18.
+ */
+
+import main.java.org.cytoscape.pokemeow.internal.algebra.*;
+
+public final class commonUtil {
+    public static final float CAM_ZOOM_RATE = 1.25F;
+    public static final Vector2 CAM_CLIP_RANGE = new Vector2(1e-2f, 1e6f);
+    public static final Vector2 CAM_VIEWPORT_SIZE = new Vector2(100, 100);
+}
