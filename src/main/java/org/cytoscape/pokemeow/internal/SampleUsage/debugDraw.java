acd69b54e950533f565bd61a96d30c35bfc392d8
ZhangMenghe <zmh.leaves@gmail.com>
2017-06-22 22:42:59 -0700

    add samples to draw all shapes

--- src/main/java/org/cytoscape/pokemeow/internal/SampleUsage/debugDraw.java
+++ src/main/java/org/cytoscape/pokemeow/internal/SampleUsage/debugDraw.java
@@ -7,17 +7,8 @@ import com.jogamp.opengl.util.texture.Texture;
 import main.java.org.cytoscape.pokemeow.internal.algebra.Vector3;
 import main.java.org.cytoscape.pokemeow.internal.algebra.Vector4;
 import main.java.org.cytoscape.pokemeow.internal.rendering.pmShaderParams;
-import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmTriangleNodeShape;
-import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmBasicNodeShape;
-import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmRectangleNodeShape;
-import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmDiamondNodeShape;
-import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmEllipseNodeShape;
-import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmParallelogramNodeShape;
-import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmCircleNodeShape;
-import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmHexagonNodeShape;
-import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmOctagonNodeShape;
-import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmVeeNodeShape;
-import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmRoundedRectangle;
+import main.java.org.cytoscape.pokemeow.internal.nodeshape. pmBasicNodeShape;
+import main.java.org.cytoscape.pokemeow.internal.nodeshape.pmNodeShapeFactory;
 import main.java.org.cytoscape.pokemeow.internal.utils.GLSLProgram;
 import main.java.org.cytoscape.pokemeow.internal.utils.pmLoadTexture;
 
@@ -28,13 +19,15 @@ public class debugDraw implements Demo {
     private int program;
     private pmBasicNodeShape[] NodeList;
 //    private pmRectangleNodeShape[] triangleNodeList;
-    private int numOfNodes=2;
+    private int numOfNodes = 10;
     private pmLoadTexture textureLoader;
     private Texture texture;
+    private pmNodeShapeFactory nodesFactory;
 
     @Override
     public void create(GL4 gl4) {
         NodeList = new pmBasicNodeShape[numOfNodes];
+        nodesFactory = new pmNodeShapeFactory(gl4);
 
         textureLoader = new pmLoadTexture();
         texture = textureLoader.initialTexture(gl4, debugDraw.class.getResource("Texture.jpg"));
@@ -44,45 +37,35 @@ public class debugDraw implements Demo {
                 null,null,null,
                 debugDraw.class.getResource("shader/texture.frag"));
         gshaderParam = new pmShaderParams(gl4,program);
+        int n=0;
+        for(Byte idx = 0;idx<10;idx++)
+            NodeList[n++] = nodesFactory.createNode(gl4, idx);
 
-        NodeList[0] = new pmRoundedRectangle(gl4);
-        NodeList[1] = new pmVeeNodeShape(gl4);
+//        NodeList[1] = nodesFactory.createNode(gl4, pmNodeShapeFactory.SHAPE_VEE);
 
         gl4.glUseProgram(program);
-
-        Vector4 [] test = {new Vector4(1.0f,.0f,.0f,1.0f),new Vector4(1.0f,.0f,.0f,1.0f)};
+        for(int x=0;x<3;x++){
+            for(int y=0;y<3;y++){
+                float cx = -0.6f +y*0.6f;
+                float cy = -0.6f+ x*0.5f;
+                int idx = 3*x+y;
+                NodeList[idx].setOrigin(new Vector3(cx,cy,.0f));
+            }
+        }
+        NodeList[9].setOrigin(new Vector3(.0f,0.8f,.0f));
+        NodeList[9].setRotation((float) Math.PI/8);
+        Vector4 [] test = {new Vector4(1.0f,.0f,.0f,1.0f),new Vector4(.0f,.0f,1.0f,1.0f)};
         NodeList[0].setColor(gl4, test);
+//
+        NodeList[3].setDefaultTexcoord(gl4);
+        NodeList[4].setDefaultTexcoord(gl4);
+//        NodeList[1].setOrigin(new Vector3(0.5f,.0f,.0f));
 
-        NodeList[1].setDefaultTexcoord(gl4);
-        NodeList[1].setOrigin(new Vector3(1.5f,.0f,.0f));
-        //NodeList[0].setRotation((float) Math.PI/8);
     }
 
     @Override
     public void render(GL4 gl4) {
-        for(int i=0;i<numOfNodes;i++){
-            gl4.glActiveTexture(GL4.GL_TEXTURE0);
-            texture.enable(gl4);
-            texture.bind(gl4);
-
-            gl4.glUniform1i(gshaderParam.sampler_texture,0);
-
-            gl4.glUniformMatrix4fv(gshaderParam.mat4_modelMatrix, 1,false, Buffers.newDirectFloatBuffer(NodeList[i].modelMatrix.asArrayCM()));
-            gl4.glUniformMatrix4fv(gshaderParam.mat4_viewMatrix, 1,false, Buffers.newDirectFloatBuffer(NodeList[i].viewMattrix.asArrayCM()));
-            gl4.glBindVertexArray(NodeList[i].gsthForDraw.objects[NodeList[i].gsthForDraw.VAO]);
-            gl4.glBindBuffer(GL_ARRAY_BUFFER, NodeList[i].gsthForDraw.objects[NodeList[i].gsthForDraw.VBO]);
-            if(i==3)
-                gl4.glDrawArrays(GL4.GL_TRIANGLES, 0, NodeList[i].numOfVertices);
-
-            else{
-//                gl4.glDrawArrays(GL4.GL_QUADS,0,4);
-                gl4.glBindBuffer(GL_ARRAY_BUFFER, NodeList[i].gsthForDraw.objects[NodeList[i].gsthForDraw.EBO]);
-                gl4.glDrawElements(GL4.GL_TRIANGLES,NodeList[i].gsthForDraw.numOfIndices, GL.GL_UNSIGNED_INT,0);
-                gl4.glBindBuffer(GL.GL_ARRAY_BUFFER,0);
-            }
-            gl4.glBindVertexArray(0);
-        }
-
+        nodesFactory.drawNodeList(gl4,NodeList,gshaderParam,texture);
     }
 
     @Override
